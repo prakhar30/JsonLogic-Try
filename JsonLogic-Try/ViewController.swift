123cb24bcb6a5d284c87e33112bea53d4862bb1b
Prakhar Tripathi <prakhar.tripathi@mirumagency.com>
2019-12-15 00:33:14 +0530

    tried sample json rules

--- JsonLogic-Try/ViewController.swift
+++ JsonLogic-Try/ViewController.swift
@@ -7,10 +7,41 @@
 //
 
 import UIKit
+import jsonlogic
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        evaluateSampleRules()
+    }
+    
+    func evaluateSampleRules() {
+        let rule = """
+          {"and" : [
+            { ">" : [3,1] },
+            { "<" : [1,3] }
+          ] }
+        """
+        
+        let rule2 = """
+        { "and" : [
+          {"<" : [ { "var" : "temp" }, 110 ]},
+          {"==" : [ { "var" : "pie.filling" }, "apple" ] }
+        ] }
+        """
+        let data = """
+          { "temp" : 100, "pie" : { "filling" : "apple" } }
+        """
+        
+        do {
+            let result: Bool = try applyRule(rule)
+            let result2: Bool = try applyRule(rule2, to: data)
+            
+            print(result)
+            print(result2)
+        } catch (let error as NSError) {
+            print(error)
+        }
     }
 }
