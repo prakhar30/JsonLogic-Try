a8db11aa34b94659ca4a545059c09887fdbf432d
Prakhar Tripathi <prakhar.tripathi@mirumagency.com>
2019-12-15 00:10:55 +0530

    first commit

---
+++ JsonLogic-Try/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  JsonLogic-Try
+//
+//  Created by Prakhar Tripathi on 15/12/19.
+//  Copyright © 2019 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# JsonLogic-Try
